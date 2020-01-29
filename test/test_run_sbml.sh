e2daa4053e4a65aef730b0befb6da6156776dd53
mdulac <melchior.dulac@inra.fr>
2020-01-29 20:10:19 +0100

    working version

--- test/test_run_sbml.sh
+++ test/test_run_sbml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -v ${PWD}/inside_run.sh:/home/inside_run.sh -v ${PWD}/tool_rpFBA.py:/home/tool_rpFBA.py -v ${PWD}/test_input.tar:/home/test_input.tar -v ${PWD}/test_inSBML.sbml:/home/test_inSBML.sbml -v ${PWD}/results/:/home/results/ --rm brsynth/rpfba /bin/sh /home/inside_run.sh
+docker run -v ${PWD}/inside_run_sbml.sh:/home/inside_run_sbml.sh -v ${PWD}/tool_rpFBA.py:/home/tool_rpFBA.py -v ${PWD}/test_rpCofactors.sbml:/home/test_rpCofactors.sbml -v ${PWD}/test_inSBML.sbml:/home/test_inSBML.sbml -v ${PWD}/results/:/home/results/ --rm brsynth/rpfba /bin/sh /home/inside_run_sbml.sh
 
 cp results/test_output.tar .
 
