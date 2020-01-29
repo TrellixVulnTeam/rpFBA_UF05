c0c0ea329cb4921a36a55847a1e47eb72dd7a922
mdulac <melchior.dulac@inra.fr>
2020-01-29 04:18:22 +0100

    small changes

--- tool_rpFBA.py
+++ tool_rpFBA.py
@@ -5,6 +5,9 @@ Created on September 21 2019
 @author: Melchior du Lac
 @description: Galaxy script to query rpFBA REST service
 
+
+python tool_rpFBA.py -inputTar test_input.tar -inSBML test_inSBML.sbml -sim_type fraction -reactions biomass RP1_sink -coefficients 0 0 -isMax True -fraction_of 0.95 -outputTar test_output.tar -dontMerge False -pathway_id rp_pathway -compartment_id MNXC3 -fill_orphan_species False
+
 """
 import argparse
 import sys
