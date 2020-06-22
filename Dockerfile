f82fafeabc98d14abeb36090c3f5b0f6c35ed8ed
mdulac <melchior.dulac@inra.fr>
2020-06-22 14:59:11 +0200

    added the galaxy

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM brsynth/rpbase:dev
 RUN pip install --no-cache-dir cobra
 COPY rpTool.py /home/
 COPY rpToolServe.py /home/
-COPY tool_rpFBA.py /home/
+COPY galaxy/tool_rpFBA.py /home/
--- rpToolServe.py
+++ rpToolServe.py
@@ -19,13 +19,11 @@ import rpTool as rpFBA
 import rpSBML
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    #level=logging.DEBUG,
+    level=logging.WARNING,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%d-%m-%Y %H:%M:%S',
 )
-
-#logging.disable(logging.INFO)
-#logging.disable(logging.WARNING)
 
 ###################################################################################
 ################################## processify #####################################
