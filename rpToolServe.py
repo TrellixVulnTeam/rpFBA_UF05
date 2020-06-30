36b950d0cfe67a84cef632f00d90526dbb619f1d
mdulac <melchior.dulac@inra.fr>
2020-07-01 01:44:30 +0200

    last switch

--- rpToolServe.py
+++ rpToolServe.py
@@ -19,8 +19,9 @@ import rpTool as rpFBA
 import rpSBML
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    #level=logging.DEBUG,
     #level=logging.WARNING,
+    level=logging.ERROR,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%d-%m-%Y %H:%M:%S',
 )
